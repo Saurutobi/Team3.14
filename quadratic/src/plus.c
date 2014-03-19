4961cd482de70ab4d1311404fd106603e30dbe61
Jason Pearson <jjpearson8611@gmail.com>
2014-03-19 12:45:11 -0400

    splitting some things up

---
+++ quadratic/src/plus.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+float QuadraticPlus(float a, float b, float c)
+{
+	float x1;
+	
+	if((pow(b, 2) - 4. * a * c) > 0)
+		x1 = (-b + sqrt(pow(b, 2) - 4 * a * c)) / (2 * a);
+	
+	else
+		printf("The result for x1 is a non-real number\n");
+	
+	return x1;
+}
