9838d7662c42c1e5837b1867c0e34334f5174234
Jason Pearson <jjpearson8611@gmail.com>
2014-03-19 12:51:35 -0400

    working still

--- quadratic/src/quadratic.c
+++ quadratic/src/quadratic.c
@@ -62,37 +62,3 @@ int main()
 	//Grab a beer, job well done
 	return 0;
 }
-
-//---------------------------------
-// Calculate the addition 
-// part of the quadratic formula
-//---------------------------------
-float QuadraticPlus(float a, float b, float c)
-{
-	float x1;
-	
-	if((pow(b, 2) - 4. * a * c) > 0)
-		x1 = (-b + sqrt(pow(b, 2) - 4 * a * c)) / (2 * a);
-	
-	else
-		printf("The result for x1 is a non-real number\n");
-	
-	return x1;
-}
-
-//---------------------------------
-// Calculate the subtraction 
-// part of the quadratic formula
-//---------------------------------
-float QuadraticMinus(float a, float b, float c)
-{
-	float x2;
-	
-	if((pow(b, 2) - 4. * a * c) > 0)
-		x2 = (-b - sqrt(pow(b, 2) - 4 * a * c)) / (2 * a);
-
-	else
-		printf("The result for x2 is a non-real number\n");
-		
-	return x2;
-} 
