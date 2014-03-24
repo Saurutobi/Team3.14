cb754ec40de00ae8cd1e8bae38379ef9a4331d82
bawnanas <djeyeless@gmail.com>
2014-03-24 15:59:16 -0400

    Update buildings.sql

--- Spikes/webaccess/db/buildings.sql
+++ Spikes/webaccess/db/buildings.sql
@@ -1,4 +1,11 @@
-CREATE TABLE `j5koehler1`.`buildings` (
-  `BuildingID` INT NOT NULL AUTO_INCREMENT,
-  `BuildingName` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`BuildingID`));
+CREATE TABLE `buildings` (
+  `BuildingID` int(11) NOT NULL AUTO_INCREMENT,
+  `BuildingDescription` varchar(100) NOT NULL,
+  `AddressLine1` varchar(100) DEFAULT NULL,
+  `AddressLine2` varchar(100) DEFAULT NULL,
+  `City` varchar(100) DEFAULT NULL,
+  `State` varchar(2) DEFAULT NULL,
+  `Zip` varchar(9) DEFAULT NULL,
+  PRIMARY KEY (`BuildingID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
