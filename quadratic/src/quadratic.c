207711c6047a79a72363628850ae2a337d143483
Jason Pearson <jjpearson8611@gmail.com>
2014-03-26 11:56:21 -0400

    added some more stuff for quadratic program

--- quadratic/src/quadratic.c
+++ quadratic/src/quadratic.c
@@ -30,6 +30,7 @@ run in shell script and return the values
 
 float QuadraticPlus(float, float, float);
 float QuadraticMinus(float, float, float);
+float GetAValue(char letter);
 
 int main()
 {
@@ -37,20 +38,21 @@ int main()
 	float x1, x2;
 	bool goodData = true;	//assume data is good
 	
-	printf("Enter values for a, b, c separated by spaces: ");
-	scanf("%f %f %f", &a, &b, &c);
-	
+	a = GetAValue('a');
+	b = GetAValue('b');
+	c = GetAValue('c');
+
 	//Data validation
-	if (a == 0.0)
-	{
-		goodData = false;
-		do{
-			printf("\"a\" can't be equal to 0. Try again: ");
-			scanf("%f", &a);
-			if (a == 0.0)
-				goodData = true;
-		}while (goodData = false);
-	}
+//	if (a == 0.0)
+//	{
+//		goodData = false;
+//		do{
+//			printf("\"a\" can't be equal to 0. Try again: ");
+//			scanf("%f", &a);
+//			if (a == 0.0)
+//				goodData = true;
+//		}while (goodData = false);
+//	}
 	
 	//Calculate them bad boys
 	x1 = QuadraticPlus(a, b, c);
