f16c0f15d31a75d7f00ee7996375492d7869c64e
Jason Pearson <jjpearson8611@gmail.com>
2014-03-26 11:00:19 -0400

    More unit testing

---
+++ quadratic/src/t2.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include "cunit.h"
+
+
+float QuadraticPlus(float a, float b, float c);
+
+int main() {
+
+float a;
+float b;
+float c;
+float result;
+// initialize the unit testing framework
+cunit_init();
+
+//  A bad unit tet
+a = 1;
+b = 5;
+c = 6;
+result = QuadraticPlus(a,b,c);
+assert_feq("ret",result,-33.0);
+
+//same variables but it passes
+a = 1;
+b = 5;
+c = 6;
+result = QuadraticPlus(a,b,c);
+assert_feq("ret",result,-2.0);
+
+//barely fails
+a = 2.1;
+b = 3.2;
+c = 1.2;
+result = QuadraticPlus(a,b,c);
+assert_feq("ret",result,-0.6666665673255922);
+
+
+//barely pass
+a = 2.1;
+b = 3.2;
+c = 1.2;
+result = QuadraticPlus(a,b,c);
+assert_feq("ret",result,-0.6666665673255920);
+
+
+exit(0);
+}
